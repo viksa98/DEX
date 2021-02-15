46554bca6e8f59c2a6f22ab20672904d72759b1b
Viktor Andonovic <andonovic.viksa@gmail.com>
2021-02-15 13:28:20 +0100

    enota

--- myapp/forms.py
+++ myapp/forms.py
@@ -24,8 +24,18 @@ class DexForm2(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         occupations = pd.read_excel(os.path.join(settings.DATA_ROOT, "SKP_ESCO.xlsx"))
-        vals = occupations["SKP poklic"].drop_duplicates()
-        choices = list(zip(vals, vals))
+        path = "C:/Users/Viktor/Desktop/pavle/viktor/hecat_django/data/"
+        ue = pd.read_csv(os.path.join(path, "sifUpravneEnote.csv"))
+        ue = ue[ue.StatusSF == 'A']
+        vals = occupations["SKP koda-4"]
+        vals1 = occupations["SKP poklic"].drop_duplicates()
+        idup = ue["IDupEnote"]
+        naziv = ue["Naziv"]
+        choices = list(zip(vals, vals1))
+        choices2 = list(zip(idup, naziv))
         self.fields["skp_code"] = forms.ChoiceField(
             choices=choices, widget=forms.Select(attrs={"class": "form-control"})
+        )
+        self.fields["up_enota"] = forms.ChoiceField(
+            choices=choices2, widget=forms.Select(attrs={"class": "form-control"})
         )
